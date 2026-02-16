05cea5a69107106791e789d8144953d8f7ff7fdf
Justin Roberts <jayviator@gmail.com>
2026-02-15 23:02:37 -0800

    Module 6 completed baseline

---
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Trip } from '../models/trip';
+import { TripCard } from '../trip-card/trip-card';
+import { TripData } from '../services/trip-data';
+import { Router } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core'; // Change: Import ChangeDetectorRef for manual change detection 
+// due to Hot Reload issues displaying blank page
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [CommonModule, TripCard],
+  templateUrl: './trip-listing.html',
+  styleUrl: './trip-listing.css',
+  providers: [TripData]
+})
+export class TripListing implements OnInit {
+  trips!: Trip[]; 
+  message: string = '';
+
+
+  
+
+
+constructor(
+  private tripData: TripData, private cdr: ChangeDetectorRef, private router: Router) { // Change: Inject ChangeDetectorRef for manual change detection
+
+  console.log('trip-listing constructor');
+}
+
+public addTrip(): void {
+  this.router.navigate(['/add-trip']);
+}
+
+private getStuff(): void {
+    this.tripData.getTrips().subscribe({
+      next: (value: any) => {
+        this.trips = value;
+        if (value.length > 0) {
+          this.message = 'There are ' + value.length + ' trips available.';
+        } 
+        else {
+          this.message = 'There are no trips retrieved from the database.';
+        } 
+        console.log(this.message);
+      this.cdr.detectChanges(); // <-- force template update
+    },
+    error: (error: any) => {
+        console.error('Error: ' + error);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.getStuff();
+  }
+
+}
+
+
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const tripsController = require('../controllers/trips');
 
 router.route('/trips')
-    .get(tripsController.tripsList); //GET Method routes tripsList
+    .get(tripsController.tripsList) //GET Method routes tripsList
+    .post(tripsController.tripsAddTrip); //POST Method routes Adds a Trip
 
 // Get Method routes tripsFindByCode - requires parameter tripCode
+// PUT Method routes Update Trip - requires parameter tripCode
 router.route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip); 
 
 module.exports = router;
